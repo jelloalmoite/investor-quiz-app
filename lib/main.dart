09a3c5196d18366b852ff311cc9cca06ec8054e2
jelloalmoite <jelloalmoite@gmail.com>
2022-05-30 12:06:38 +0800

    Added loading screen & validation double-tap exit

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,31 @@ import '/pages/home.dart';
 import '/pages/profile.dart';
 import '/pages/settings.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('historyBox');
   runApp(const MyApp());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Image.asset('assets/images/logo_animated.gif',
+          height: 280, width: 280),
+      backgroundColor: Colors.white,
+      nextScreen: const MainPage(),
+      splashIconSize: 280,
+      duration: 3000, //1000 = 1 sec (default 2500)
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -28,7 +47,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: const Color.fromRGBO(254, 254, 254, 1),
       ),
-      home: const MainPage(),
+      home: const SplashScreen(),
+      //Kung debugging pwede paltan home ng MainPage()
     );
   }
 }
@@ -48,45 +68,70 @@ class _MainPageState extends State<MainPage> {
     const ProfilePage(),
     const SettingsPage(),
   ];
+  DateTime? lastPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: selectedIndex,
-        children: screens,
-      ),
-      bottomNavigationBar: NavigationBarTheme(
-        data: NavigationBarThemeData(
-          indicatorColor: const Color.fromRGBO(81, 231, 168, 1),
-          labelTextStyle: MaterialStateProperty.all(const TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 14,
-              fontWeight: FontWeight.bold)),
+    return WillPopScope(
+      onWillPop: () async {
+        final now = DateTime.now();
+        final maxDuration = Duration(seconds: 2);
+        final isWarning =
+            lastPressed == null || now.difference(lastPressed!) > maxDuration;
+
+        if (isWarning) {
+          lastPressed = DateTime.now();
+          final snackBar = SnackBar(
+            content: Text('Double Tap to Close App'),
+            duration: maxDuration,
+          );
+
+          ScaffoldMessenger.of(context)
+            ..removeCurrentSnackBar()
+            ..showSnackBar(snackBar);
+
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        body: IndexedStack(
+          index: selectedIndex,
+          children: screens,
         ),
-        child: NavigationBar(
-          height: 70,
-          selectedIndex: selectedIndex,
-          onDestinationSelected: (index) =>
-              setState(() => selectedIndex = index),
-          destinations: const [
-            NavigationDestination(
-                icon: Icon(Icons.home_outlined),
-                selectedIcon: Icon(Icons.home_rounded),
-                label: 'Home'),
-            NavigationDestination(
-                icon: Icon(Icons.content_paste_rounded),
-                selectedIcon: Icon(Icons.content_paste_search_rounded),
-                label: 'History'),
-            NavigationDestination(
-                icon: Icon(Icons.person_outline_rounded),
-                selectedIcon: Icon(Icons.person_rounded),
-                label: 'Profile'),
-            NavigationDestination(
-                icon: Icon(Icons.settings_outlined),
-                selectedIcon: Icon(Icons.settings_rounded),
-                label: 'Settings'),
-          ],
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: const Color.fromRGBO(81, 231, 168, 1),
+            labelTextStyle: MaterialStateProperty.all(const TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 14,
+                fontWeight: FontWeight.bold)),
+          ),
+          child: NavigationBar(
+            height: 70,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) =>
+                setState(() => selectedIndex = index),
+            destinations: const [
+              NavigationDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home_rounded),
+                  label: 'Home'),
+              NavigationDestination(
+                  icon: Icon(Icons.content_paste_rounded),
+                  selectedIcon: Icon(Icons.content_paste_search_rounded),
+                  label: 'History'),
+              NavigationDestination(
+                  icon: Icon(Icons.person_outline_rounded),
+                  selectedIcon: Icon(Icons.person_rounded),
+                  label: 'Profile'),
+              NavigationDestination(
+                  icon: Icon(Icons.settings_outlined),
+                  selectedIcon: Icon(Icons.settings_rounded),
+                  label: 'Settings'),
+            ],
+          ),
         ),
       ),
     );
