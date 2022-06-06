fcdbd2ca0e8570a25d443b51bdf9c6c1b98398a7
jelloalmoite <jelloalmoite@gmail.com>
2022-06-06 16:47:17 +0800

    Adjusted design on categorypage & home + Locking of categories

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import '/pages/categorypage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -33,6 +34,77 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  categoryLock(String title, VoidCallback func) {
+    if (title == 'Personal Finance') {
+      return func;
+    } else if (title == 'Investment and Portfolio Management' &&
+        catLevel >= 100) {
+      return func;
+    } else if (title == 'Behavioral Finance' && catLevel >= 200) {
+      return func;
+    } else if (title == 'Capital Markets' && catLevel >= 300) {
+      return func;
+    } else {
+      return null;
+    }
+  }
+
+  Widget category(String title, String image, Color color1, Color color2,
+      VoidCallback func) {
+    return Container(
+      height: 210,
+      width: 160,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: color1,
+            offset: const Offset(-3.0, 4.0),
+            blurRadius: 10.0,
+          ),
+        ],
+      ),
+      child: MaterialButton(
+        onPressed: categoryLock(title, func),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: AutoSizeText(title,
+                    maxLines: 3,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontFamily: 'Poppins-Black',
+                      fontWeight: FontWeight.w900,
+                      height: 1.1,
+                      letterSpacing: 1,
+                    )),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Image(
+                  image: AssetImage(image),
+                  height: 130,
+                  width: 130,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -50,9 +122,10 @@ class _HomePageState extends State<HomePage> {
               margin: const EdgeInsets.fromLTRB(0, 8, 15, 8),
               padding: const EdgeInsets.fromLTRB(5, 0, 10, 0),
               decoration: BoxDecoration(
+                color: const Color.fromRGBO(81, 231, 168, 0.4),
                 borderRadius: BorderRadius.circular(10),
                 border: Border.all(
-                  color: const Color.fromRGBO(5, 195, 107, 50),
+                  color: const Color.fromRGBO(81, 231, 168, 1),
                   width: 3,
                 ),
               ),
@@ -79,115 +152,75 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            const Text('Welcome!',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                )),
-            const Text('Find a course you want to learn',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontFamily: 'Poppins',
-                )),
-            const Padding(padding: EdgeInsets.all(6.0)), //Space between rows
+            RichText(
+              textAlign: TextAlign.left,
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                      text: 'Welcome!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontFamily: 'Poppins-Medium',
+                        fontWeight: FontWeight.w900,
+                        height: 0,
+                        letterSpacing: 1,
+                      )),
+                  TextSpan(
+                      text: '\nFind a course you want to learn',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        height: 2,
+                        letterSpacing: 0.5,
+                      )),
+                ],
+              ),
+            ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Material(
-                  //color: Colors.green,
-                  //elevation: 8,
-                  borderRadius: BorderRadius.circular(15),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    onTap: () => {toCategoryPage(1)},
-                    child: Ink.image(
-                      image: const AssetImage('assets/images/personal.png'),
-                      height: 210,
-                      width: 160,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
+                category(
+                  'Personal Finance',
+                  'assets/images/personal_finance.png',
+                  const Color.fromRGBO(83, 215, 80, 1),
+                  const Color.fromRGBO(186, 229, 61, 1),
+                  () => {toCategoryPage(1)},
                 ),
-                //const Padding(padding: EdgeInsets.all(5.0)),    //Space between columns
-                Material(
-                  //color: Colors.purple,
-                  //elevation: 8,
-                  borderRadius: BorderRadius.circular(15),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    onTap: () => {
-                      if (catLevel > 100)
-                        {toCategoryPage(2)}
-                      else if (catLevel < 100)
-                        {print("Need to finish Personal Finance first")}
-                    },
-                    child: Ink.image(
-                      image: const AssetImage('assets/images/investment.png'),
-                      height: 210,
-                      width: 160,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
+                category(
+                  'Investment and Portfolio Management',
+                  'assets/images/investment_and_portfolio_management.png',
+                  const Color.fromRGBO(213, 162, 244, 1),
+                  const Color.fromRGBO(81, 90, 218, 1),
+                  () => {toCategoryPage(2)},
                 ),
               ],
             ),
-            const Padding(padding: EdgeInsets.all(6.0)), //Space between rows
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Material(
-                  //color: Colors.red,
-                  //elevation: 8,
-                  borderRadius: BorderRadius.circular(15),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    onTap: () => {
-                      if (catLevel > 200)
-                        {toCategoryPage(3)}
-                      else if (catLevel < 200)
-                        {
-                          print(
-                              "Need to finish Investment and Management first")
-                        }
-                    },
-                    child: Ink.image(
-                      image: const AssetImage('assets/images/behavioral.png'),
-                      height: 210,
-                      width: 160,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
+                category(
+                  'Behavioral Finance',
+                  'assets/images/behavioral_finance.png',
+                  const Color.fromRGBO(255, 143, 163, 0.86),
+                  const Color.fromRGBO(215, 32, 32, 1),
+                  () => {toCategoryPage(3)},
                 ),
-                //const Padding(padding: EdgeInsets.all(5.0)),    //Space between columns
-                Material(
-                  //color: Colors.deepOrange,
-                  //elevation: 8,
-                  borderRadius: BorderRadius.circular(15),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    onTap: () => {
-                      if (catLevel > 300)
-                        {toCategoryPage(4)}
-                      else if (catLevel < 300)
-                        {print("Need to finish Behavioral Finance first")}
-                    },
-                    child: Ink.image(
-                      image: const AssetImage('assets/images/capital.png'),
-                      height: 210,
-                      width: 160,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
+                category(
+                  'Capital Markets',
+                  'assets/images/capital_markets.png',
+                  const Color.fromRGBO(241, 106, 47, 1),
+                  const Color.fromRGBO(240, 243, 60, 1),
+                  () => {toCategoryPage(4)},
                 ),
               ],
             ),
